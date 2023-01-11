bffa39a0c1eb304d968f3fff34fd49082f8bd89c
dante <heywheredante@gmail.com>
2023-01-12 00:37:30 +0900

    GXCK-19 날씨 scrollView

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>잘되네? 뭐지 </Text>
-      <StatusBar style="auto" />
+  return <View style={styles.container}>
+    <View style={styles.city}>
+      <Text style={styles.cityName}>서울</Text>
     </View>
-  );
+    <ScrollView 
+      pagingEnabled 
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={styles.weather}>
+
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+    </ScrollView>
+  </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1, 
+    backgroundColor:"purple",
   },
-});
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+    color: "white"
+  },
+  weather: {
+  },
+  day : {
+    width: SCREEN_WIDTH,
+    alignItems:"center",    
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+})
